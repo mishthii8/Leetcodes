1635f73a5683427ddd346bc65683dfdffd0000ab
Mishthi <mishthidutta7@gmail.com>
2026-08-03 01:18:02 +0530

    Roman to Integer

---
+++ Maths/romantoint.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char,int>
+    mp= {
+        {'I',1},
+        {'V',5},
+        {'X',10},
+        {'L',50},
+        {'C',100},
+        {'D',500},
+        {'M',1000},
+    };
+    int a=0;
+    for (int i=0; i < s.length(); i++){
+
+        if (i+1 < s.length() && mp[s[i]] < mp[s[i+1]])
+          a -= mp[s[i]];
+        else 
+         a += mp[s[i]];
+    }
+     return a ;
+    }
+};
